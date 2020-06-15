b89c5ba9a1640a43fe6efbd82cd7dfbae009e133
Hassan Nasser <eng87hassannasser@gmail.com>
2020-06-15 14:54:28 +0200

    Aufgabe1+2

---
+++ Expression.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ int main() {
+     int a;
+     int b;
+     int c;
+     int d;
+
+      scanf("%d", &a);
+      scanf("%d", &b);
+      scanf("%d", &c);
+      scanf("%d", &d);
+    
+     int ergebnis = (a*b)+(a*c)
+     printf("Das Ergebnis ist: %d", ergebnis);
+     return 0 ;
+ }
---
+++ signum.c
@@ -0,0 +1,20 @@
+include <stdio.h>
+int signum (int a){
+    if if (a < 0) {
+printf("-1\n");
+ }
+
+  if (a > 0) {
+ printf("1\n")
+}
+if (a == 0){
+    printf("0\n")
+}}
+int main (){
+    int b ;
+    scanf (scanf("%d", &b);
+    int ergebnis = signum(b);
+
+ printf("Das Ergebnis ist: %d", ergebnis);
+
+return 0 ;}
